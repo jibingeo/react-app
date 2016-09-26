ea5ec837f4bc17bb7a22fe2ec434a9d99c88c949
Jibin George <mail@jibingeorge.com>
2016-09-26 23:25:03 +0530

    added reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import App from './components/App'
+import reducers from './reducers'
+
+let store = createStore(reducers)
+
+let component = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
 
 ReactDOM.render(
-	<App />, 
+	component, 
 	document.getElementById('container')
 );
