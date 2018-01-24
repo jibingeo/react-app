f0902a34338ed1ad88133456613ec9fbebfc9835
Jibin George <mail@jibingeorge.com>
2018-01-24 13:05:46 +0530

    upgrade fixed

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = ({ file, options, env }) => ({
+  plugins: {
+    'precss': {},
+    'autoprefixer': {}
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var precss       = require('precss');
-var autoprefixer = require('autoprefixer');
 
 const isProd = process.env.NODE_ENV==='production';
 const isDev = !isProd;
@@ -20,11 +18,18 @@ module.exports ={
 		rules: [
 			{
 				test: /\.js/,
-				loader: "babel-loader?compact=false"
+				use: ["babel-loader?compact=false"]
 			},
 			{
 				test: /\.css/,
-				loader:  "style-loader!css-loader?modules&localIdentName=[name]-[local]-[hash:base64:5]!postcss-loader"
+				use: [
+					"style-loader",
+					{
+						loader: 'css-loader',
+						options: { importLoaders: 1 }
+					},
+					'postcss-loader'
+				]
 			}
 		]
 	},
@@ -32,15 +37,6 @@ module.exports ={
 		new HtmlWebpackPlugin({
 			template: path.join(__dirname, "src/index.html"),
 			minify: isProd && { collapseWhitespace: true }
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				context: __dirname,
-				postcss: [
-					autoprefixer,
-					precss
-				]
-			}
 		})
 	]
 };
