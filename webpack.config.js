2c870faa691a14f26778cb737f35fd26654e1f3c
Jibin George <mail@jibingeorge.com>
2018-01-25 00:25:40 +0530

    change compose file

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,48 @@ var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProd = process.env.NODE_ENV==='production';
+const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
-
+const PORT = process.env.PORT || 8080;
 const chunkHash = isProd ? '[chunkhash:8]' : '[name]';
 
-module.exports ={
-	entry: "./src/index.js",
+module.exports = {
+	entry: './src/index.js',
 	output: {
-		path: path.join(__dirname, "dist"),
+		path: path.join(__dirname, 'dist'),
 		filename: `${chunkHash}.js`,
-		chunkFilename: `${chunkHash}.js`
+		chunkFilename: `${chunkHash}.js`,
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js/,
-				use: ["babel-loader?compact=false"]
+				use: ['babel-loader?compact=false'],
 			},
 			{
 				test: /\.css/,
 				use: [
-					"style-loader",
+					'style-loader',
 					{
 						loader: 'css-loader',
-						options: { importLoaders: 1 }
+						options: { importLoaders: 1 },
 					},
-					'postcss-loader'
-				]
+					'postcss-loader',
+				],
 			},
 			{
 				test: /\.jade$/,
-				use: ['jade-loader']
-			}
-		]
+				use: ['jade-loader'],
+			},
+		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.join(__dirname, "src/index.jade")
-		})
+			template: path.join(__dirname, 'src/index.jade'),
+		}),
 	],
 	devServer: {
-		port: 8080,
+		port: PORT,
 		host: '0.0.0.0',
 	},
 };
